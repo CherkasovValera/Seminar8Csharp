633c08f06e88bbef54de586a06f0ae7a615506e6
Valeriy <cherkasov77@mail.ru>
2022-09-04 15:38:20 +0400

    54и56и58задачиСеминар8

--- Program.cs
+++ Program.cs
@@ -79,6 +79,68 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+// Console.Clear();
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+// int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
+// {
+//     int[,] matrix = new int[numLine, numColumns];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(minRand, maxRand);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"\t{matrix[i, j],6}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int numLine = Prompt("Введите количество строк массива ");
+// int numColumns = Prompt("Введите количество столбцов массива ");
+// int maxRand = Prompt("Введите верхнюю границу диапазона ");
+// int minRand = Prompt("Введите нижнюю границу диапазона ");
+// int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
+// PrintArray(matrix);
+// int stringsum = 0;
+// int sumMin = numLine * numColumns * maxRand;
+// int numstring = 0;
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int n = 0; n < matrix.GetLength(1); n++)
+//         stringsum += matrix[i, n];
+//     if (stringsum < sumMin)
+//     {
+//         sumMin = stringsum;
+//         numstring = i + 1;
+//     }
+//     stringsum = 0;
+// }
+
+// System.Console.WriteLine($"Минимум в строке  {numstring} равен  {sumMin}");
+
+
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
 Console.Clear();
 int Prompt(string message)
 {
@@ -93,7 +155,7 @@ int[,] FillArray(int numLine, int numColumns, int maxRand, int minRand)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(minRand, maxRand);
+            matrix[i, j] = new Random().Next(minRand, maxRand + 1);
         }
     }
     return matrix;
@@ -109,34 +171,75 @@ void PrintArray(int[,] matrix)
         Console.WriteLine();
     }
 }
-int numLine = Prompt("Введите количество строк массива ");
-int numColumns = Prompt("Введите количество столбцов массива ");
-int maxRand = Prompt("Введите верхнюю границу диапазона ");
-int minRand = Prompt("Введите нижнюю границу диапазона ");
-int[,] matrix = FillArray(numLine, numColumns, maxRand, minRand);
-PrintArray(matrix);
-int stringsum = 0;
-int sumMin = numLine * numColumns * maxRand;
-int numstring = 0;
-for (int i = 0; i < matrix.GetLength(0); i++)
+int numLine1 = Prompt("Введите количество строк массива №1 ");
+int numColumns1 = Prompt("Введите количество столбцов массива №1 ");
+int maxRand1 = Prompt("Введите верхнюю границу диапазона №1 ");
+int minRand1 = Prompt("Введите нижнюю границу диапазона №1 ");
+int[,] matrix1 = FillArray(numLine1, numColumns1, maxRand1, minRand1);
+PrintArray(matrix1);
+int numLine2 = Prompt("Введите количество строк массива №2 ");
+int numColumns2 = Prompt("Введите количество столбцов массива №2 ");
+int maxRand2 = Prompt("Введите верхнюю границу диапазона №2 ");
+int minRand2 = Prompt("Введите нижнюю границу диапазона №2 ");
+int[,] matrix2 = FillArray(numLine2, numColumns2, maxRand2, minRand2);
+PrintArray(matrix2);
+
+int[,] SumMatrix(int newLine, int newColumns)
 {
-    for (int n = 0; n < matrix.GetLength(1); n++)
-        stringsum += matrix[i, n];
-    if (stringsum < sumMin)
+    int[,] matrix = new int[newLine, newColumns];
+    for (int i = 0; i < numLine1; i++)
     {
-        sumMin = stringsum;
-        numstring = i + 1;
+
+        for (int j = 0; j < numColumns2; j++)
+        {
+
+            for (int k = 0; k < numLine2; k++)
+            {
+                matrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
     }
-    stringsum = 0;
+    return matrix;
 }
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"\t{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine($"Произведение двух матриц:");
+int newLine = numLine1;
+int newColumns = numColumns2;
+int[,] matrix = SumMatrix(newLine, newColumns);
+PrintMatrix(matrix);
 
-System.Console.WriteLine($"Минимум в строке  {numstring} равен  {sumMin}");
 
 
 
+// int stringsum = 0;
+// int sumMin = numLine * numColumns * maxRand;
+// int numstring = 0;
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int n = 0; n < matrix.GetLength(1); n++)
+//         stringsum += matrix[i, n];
+//     if (stringsum < sumMin)
+//     {
+//         sumMin = stringsum;
+//         numstring = i + 1;
+//     }
+//     stringsum = 0;
+// }
+
+//System.Console.WriteLine($"Минимум в строке  {numstring} равен  {sumMin}");
 
 
-// int[,] matrix = new int[3, 3];
+// 
 
 // FillMatrix(matrix);
 // PrintArray(matrix);
